630d9b58d741834b5a4d1a1098708a89260cd017
lrybakova <63198490+lrybakova@users.noreply.github.com>
2020-04-18 20:04:36 +0300

    Plugins exercise
    Installed ESLint, File loader and HTMLWebpackPlugin
    Changed the output from main to bundle, because now it will not only give js, but also other file formats, eg images

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: './src/client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
         test: '/\.js$/',
         exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+      {
+        enforce: 'pre',
+        test: '/\.js*/',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: "./src/client/views/index.html",
+    filename: "./index.html",
+    path: path.resolve(__dirname, './src/client/views'),
+  })
+],
 };
